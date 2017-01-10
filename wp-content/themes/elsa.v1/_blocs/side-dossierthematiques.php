a8e05860da88484c7adf665f87acf859968af986
Thomas <hello@thomasflorentin.net>
2017-01-10 13:14:13 +0100

    Remove v1 theme

--- wp-content/themes/elsa.v1/_blocs/side-dossierthematiques.php
+++
@@ -1,3 +0,0 @@
-<div class="thematique">
-            	<?php global $cnSite; $cnSite->get_bloc_text('bloc-dossiers-thematiques');?>
-            </div>
--- wp-content/themes/elsa.v1/_blocs/side-reseau.php
+++
@@ -1,6 +0,0 @@
-<div class="reseau">
-                
-                <?php global $cnSite; $cnSite->get_bloc_text('bloc-reseau');?>
-                <div class="clear"></div>
-                
-             </div>
