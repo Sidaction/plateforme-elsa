fedda2c6b16825bd8db8860241980bf49765cb24
Thomas <hello@thomasflorentin.net>
2017-01-10 13:14:32 +0100

    Remove old elsa theme

--- wp-content/themes/elsa/_blocs/side-adecouvrir.php
+++
@@ -1,5 +0,0 @@
- <div class="adecouvrir">
-        <div>&Agrave; DÉCOUVRIR AUSSI</div>
-         <?php global $cnSite; $cnSite->get_bloc_text('bloc-a-decouvrir-aussi');?>
-        <div class="clear"></div>
-        </div>
