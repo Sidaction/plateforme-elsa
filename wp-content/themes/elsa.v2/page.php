0307ed8c747cb8300783a9dae7428f4fbd90e1c3
Thomas <hello@thomasflorentin.net>
2017-01-11 11:23:21 +0100

    Markup

--- wp-content/themes/elsa.v2/page.php
+++ wp-content/themes/elsa.v2/page.php
@@ -1,25 +1,28 @@
 <?php 
-/*///////////////////////////////////////////////////////////////
- Plateforme Elsa by Clair et Net. / www.clair-et-net.com
- //////////////////////////////////////////////////////////////*/
- get_header(); ?>
+ get_header(); 
+?>
+
+
  <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-<section id="contentSite" class="orange">
-	<div id="breadcrumb">
-    	<div id="breadcrumbWrapper">Vous êtes ici » <a href="/">Accueil</a> » <a href="/plateforme-elsa/">Plateforme Elsa</a> » <a href="#"><?php the_title();?></a></div>
-    </div>
-     <div id="contentWrapper">
-     	<div class="shadowLeft"></div>
-        <div class="shadowRight"></div>
-        <article>
-        <h1><?php the_title();?></h1>
-           <?php the_content();?>
-            
-           <?php $cnSite->share_links();?>
-            
-        </article>
-        <?php get_sidebar(); ?>
-     </div>
-</section>
+ 
+    <section id="contentSite" class="orange">
+    	<div id="breadcrumb">
+        	<div id="breadcrumbWrapper">Vous êtes ici » <a href="/">Accueil</a> » <a href="/plateforme-elsa/">Plateforme Elsa</a> » <a href="#"><?php the_title();?></a></div>
+        </div>
+         <div id="contentWrapper">
+         	<div class="shadowLeft"></div>
+            <div class="shadowRight"></div>
+            <article>
+            <h1><?php the_title();?></h1>
+               <?php the_content();?>
+                
+               <?php $cnSite->share_links();?>
+                
+            </article>
+            <?php get_sidebar(); ?>
+         </div>
+    </section>
+
+
 <?php endwhile; ?>
 <?php get_footer(); ?>
