81c0ff19008c9be2fc966b8d221f824cda3c955a
Thomas <hello@thomasflorentin.net>
2017-01-12 15:29:27 +0100

    Delete junk

--- wp-content/themes/elsa.v2/_blocs/side-ressources.php
+++
@@ -1,3 +0,0 @@
-<div class="ressources"> <a href="/soumettre-une-ressource/"> Le centre de ressources<br>
-  s'enrichit aussi <strong>grâce à vous...</strong><br>
-  N'hésitez pas à nous <span>SOUMETTRE <strong>UNE RESSOURCE</strong></span> </a> </div>
