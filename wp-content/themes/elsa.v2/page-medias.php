b066317846d7368de96d28806e6bdcb0540135b4
Thomas <hello@thomasflorentin.net>
2017-01-12 11:36:18 +0100

    Add Medias template and part. Add loop. Working so far

---
+++ wp-content/themes/elsa.v2/page-medias.php
@@ -0,0 +1,89 @@
+<?php 
+
+/*
+ * Page "Nos médias"
+ * Template Name: Page archives médias
+ */
+
+get_header(); ?>
+
+ <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+
+  <section id="site-content" class="site-content single-ressource">
+
+    <article class="main-content clearfix noback">
+        
+        <div class="page_title ressource_title">
+       
+            <div class="wrap row">
+                <h1 class="h1 m-6col is-centered">
+                    <?php the_title(); ?>
+                </h1>  
+            </div>     
+        
+        </div>
+
+        <div class="page_content clearfix">
+            <div class="wrap">
+
+                <div class="row"> Pagination</div>
+
+                <div class="row">
+
+                    <?php
+
+                        $args = array(
+                            'posts_per_page' => 4,
+                            'post_type' => array('post', 'contenu'),
+                            'format' => 'video',
+                            'post_status' => 'publish'
+                        );
+                        $medias = get_posts( $args ); 
+
+                        foreach ( $medias as $post ) : setup_postdata( $post ); ?>
+
+                            <?php get_template_part('template-parts/parts/part', 'media'); ?>
+
+
+                        <?php endforeach; 
+                        wp_reset_postdata();?>
+
+
+
+
+                </div>
+
+                <div class="row"> Pagination</div>
+
+
+            </div><!-- .wrap -->
+        </div><!-- .page_content -->
+
+    </article>
+
+
+
+    <aside class="blocs_group--rebonds bg-cut">
+        <div class="wrap row">
+        
+            <div class="group_title m-2col">
+                <h3 class="A lire aussi">A lire aussi</h3>
+            </div>
+        
+            <div class="group_list">
+                <div class="group_bloc m-2col">hello</div>
+                <div class="group_bloc m-2col">hello</div>
+                <div class="group_bloc m-2col">hello</div>
+            </div>
+
+        </div>
+    </aside>
+
+
+   
+  </div>
+</section>
+
+
+<?php endwhile; ?>
+<?php get_footer(); ?>
---
+++ wp-content/themes/elsa.v2/template-parts/parts/part-media.php
@@ -0,0 +1,7 @@
+
+
+<div class="media-item">
+
+  <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+  
+</div>
