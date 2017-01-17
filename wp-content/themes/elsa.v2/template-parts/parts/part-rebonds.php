81240b009dc203fa90188f477b783336851d9b33
Thomas <hello@thomasflorentin.net>
2017-01-17 17:11:34 +0100

    Add part-rebonds

--- wp-content/themes/elsa.v2/template-parts/parts/part-rebonds.php
+++ wp-content/themes/elsa.v2/template-parts/parts/part-rebonds.php
@@ -1,46 +1,3 @@
-<?php 
-/*
- * Fiche ressource (post)
- */
- 
-
-
-  $cnSite->page_type = 'ressource';	
-  $format = cnLib::get_main_term_slug($post->ID, 'format');
-  $category = cnLib::get_main_term_slug($post->ID, 'category');
-  $link = get_post_meta($post->ID, 'link', true);
-  $link_crips = get_post_meta($post->ID, 'link_crips', true);
-  $date_edition = get_post_meta($post->ID, 'date-start', true);
-  $auteurs = $cnSite->get_authors_withlink($post->ID);
-  $tools= get_post_meta($post->ID, 'outil', true);
-
-  set_query_var( 'cnSite', $cnSite ); 
-  set_query_var( 'format', $format ); 
-  set_query_var( 'category', $category ); 
-  set_query_var( 'date_edition', $date_edition ); 
-  set_query_var( 'link', $link ); 
-  set_query_var( 'auteurs', $auteurs );     
-
-  get_header(); 
-?>
-
-
-
-<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-
-
-    <?php
-      // AIGUILLER SI MEDIAS OU RESSSOURCE "SIMPLE" (pdf, link, etc.)
-      if( has_term( 'video', 'format' ) || has_term( 'diaporama', 'format' ) ) {
-          get_template_part('template-parts/content', 'media');
-      }
-      else {
-        get_template_part('template-parts/content', 'ressource');
-      }
-    ?>
-
-
-
     <?php $rebonds = get_field('rebonds_default', 'option'); ?>
     <?php if( $rebonds) : ?>
     <aside class="blocs_group--rebonds">
@@ -99,12 +56,3 @@
       </div><!-- .row -->
     </aside>
     <?php endif; ?>
-   
-  </div>
-</section>
-
-
-
-<?php endwhile; ?>
-
-<?php get_footer(); ?>
