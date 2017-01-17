172b3d6ef3928224ed587f86debfa9b420d6cfbc
Thomas <hello@thomasflorentin.net>
2017-01-17 18:02:17 +0100

    Markup & Polish Annuaire

--- wp-content/themes/elsa.v2/page-annuaire.php
+++ wp-content/themes/elsa.v2/page-annuaire.php
@@ -12,10 +12,18 @@
     'order' => 'ASC',
     'type_structure' => 'partenaires-elsa-associations-du-reseau-elsa');
  
-  $args['pays_assoc'] =  $_GET['pays_assoc'];
-  $args['activites'] = $_GET['activites'];
-  $args['public_cibles'] = $_GET['public_cibles'];
- 
+  if( isset($_GET['pays_assoc']) ) {
+    $args['pays_assoc'] = $_GET['pays_assoc'];
+  }
+
+  if( isset($_GET['activites']) ) {
+    $args['activites'] = $_GET['activites'];
+  }
+
+  if( isset($_GET['public_cibles']) ) {
+    $args['public_cibles'] = $_GET['public_cibles'];
+  }
+
   $_SESSION['argstructures'] = $args;
   $results = array();
  
@@ -37,35 +45,40 @@
               <h1 class="h1">
                   <?php the_title();?>
               </h1>
-              Vous avez <?php echo $wp_query->found_posts;?> résultats...
           </div>     
         
         </div>
 
-        <?php the_content();?>
-
+        <div class="page_content">
+          <div class="wrap row">
+            <div class="m-4col">
+              <?php the_content();?>
+            </div>
+            <div class="page_media m-3col m-last">
+              <?php the_post_thumbnail(); ?>
+            </div>
+          </div>
+        </div>
+        
         <div class="search_filters bg-cut blocs_group">
           <div class="wrap row">
 
             <div class="group_title m-2col">
-              Filtre d'affichage
-              <div class="exportxls"><a href="../extract">Exporter au format xls</a></div>
+              <h3>Filtre d'affichage</h3>
+              <div class="exportxls"><a href="../extract" class="btn-inline">Exporter au format xls</a></div>
             </div>
             
-            <div class="m-6col">
+            <div class="">
               <form id="rech">
-                <?php cnLib::custom_taxonomy_dropdown("pays_assoc", "selectBox", "Pays",'','',false); ?>
-                <?php cnLib::custom_taxonomy_dropdown("public_cibles", "selectBox", "Publics cibles",'','',false); ?>
-                <?php cnLib::custom_taxonomy_dropdown("activites", "selectBox", "Activités",'','',false); ?>
+                <div class="m-2col">  <?php cnLib::custom_taxonomy_dropdown("pays_assoc", "selectBox", "Pays",'','',false); ?></div>
+                <div class="m-2col">  <?php cnLib::custom_taxonomy_dropdown("public_cibles", "selectBox", "Publics cibles",'','',false); ?></div>
+                <div class="m-2col">  <?php cnLib::custom_taxonomy_dropdown("activites", "selectBox", "Activités",'','',false); ?></div>
               </form>
             </div>
 
           </div>
-        </div>
 
-
-      <div class="search_list bg-cut">
-        <div class="wrap ">          
+          <div class="wrap search_list  ">          
 
             <ul class="no-bullets">
 
@@ -75,18 +88,46 @@
               
                 <?php while ($wp_query->have_posts()) : $wp_query->the_post(); 
               
-                  $email=get_post_meta($post->ID, 'email', true);
-                  $link=get_post_meta($post->ID, 'link', true);?>
+                  $email = get_post_meta($post->ID, 'email', true);
+                  $link = get_post_meta($post->ID, 'link', true);?>
 
                     <li class="search-item">
-                      <a href="<?php the_permalink();?>"><?php the_title();?></a>
+                      <a href="<?php the_permalink();?>">
+                        <h4 class="h4"><?php the_title();?></h4>
+                      </a>
+                      
+                      <div class="row">
+                        <div class="m-1col">
+                          <span class="meta"> <?php echo cnLib::get_terms_withoutlink($post->ID, "pays_assoc", $separ = ", ");?></span>
+                        </div>
+
+                        <div class="m-5col">
+                          <span class="meta">Public(s) cible(s) : </span><?php echo cnLib::get_terms_withoutlink($post->ID, "public_cibles", $separ = ", ");?>
+                          <div class="clearfix"></div>
+                          <span class="meta">Activité(s) : </span><?php echo cnLib::get_terms_withoutlink($post->ID, "activites", $separ = ", ");?>
+                        </div>
                         
-                        <?php echo cnLib::get_terms_withoutlink($post->ID, "pays_assoc", $separ = ", ");?>
-                        <?php echo cnLib::get_terms_withoutlink($post->ID, "public_cibles", $separ = ", ");?></td>
-                        <?php echo cnLib::get_terms_withoutlink($post->ID, "activites", $separ = ", ");?></td>
-                        <?php echo get_post_meta($post->ID, 'tel', true);?></td>
-                        <?php if(!empty($link)):?><a href="<?php echo $link;?>" class="webIco" target="_blank"></a><?php endif;?></td>
-                       <?php if(!empty($email)):?><a href="mailto:<?php echo $email;?>"></a><?php endif;?></td>
+                        <div class="m-2col">
+                          <ul class="no-bullets">
+
+                            <?php if(!empty(get_post_meta($post->ID, 'tel', true))):?>
+                              <li>
+                                <span class="btn-inline"><?php echo get_post_meta($post->ID, 'tel', true);?></span>
+                              </li>
+                            <?php endif;?>
+
+                            <?php if(!empty($link)):?>
+                              <li><a href="<?php echo $link;?>" class="btn-inline" target="_blank">Site internet</a></li>
+                            <?php endif;?>
+
+                            <?php if(!empty($email)):?>
+                              <li><a class="btn-inline" href="mailto:<?php echo $email;?>">Email</a></li>
+                            <?php endif;?>
+                          </ul>
+                        </div>
+                        
+                      </div>  
+
                     </li>
 
                 <?php endwhile; wp_reset_query(); wp_reset_postdata(); $args=null; ?>
@@ -94,7 +135,7 @@
 
             </ul>
 
-        </div><!-- search_list -->
+          </div><!-- search_list -->
       </div>
 
   </section><!-- .site-content -->
