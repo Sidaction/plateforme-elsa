562add9387da3919601e759ce3dd04c448bf51ce
Thomas <hello@thomasflorentin.net>
2017-01-19 18:08:40 +0100

    Add bookmark btn

--- wp-content/themes/elsa.v2/template-parts/parts/part-bloc.php
+++ wp-content/themes/elsa.v2/template-parts/parts/part-bloc.php
@@ -3,7 +3,11 @@
 
 <?php if( $type == 'media' ) : ?>
   <div class="bloc_item bloc--<?php echo $type; ?> bg_cover" style="background-image:url(<?php if ( has_post_thumbnail() ) { the_post_thumbnail_url(); }  ?>)">
-      <a href="#" class="bookmark"><span class="icon-bookmark_full"><span class="path1"></span><span class="path2"></span></span></a>
+
+      <?php //echo $gema75_ril_frontend->show_read_it_later_after_title( 'hello' ); ?>
+
+      <a href="#" class="bookmark"><span class="gema75_read_it_later_text addToReadItLaterButton" data-readitlater-id="<?php echo $post->ID; ?>"><span class="icon-bookmark_full"><span class="path1"></span><span class="path2"></span></span></span></a>
+
       <a href="<?php the_permalink();?>">
           <?php echo $auteurs = $cnSite->get_authors($post->ID); ?>
           <?php get_terms(); ?>
@@ -14,7 +18,8 @@
 
 <?php else : ?>
   <div class="bloc_item bloc--<?php echo $type; ?>">
-      <a href="#" class="bookmark"><span class="icon-bookmark_full"><span class="path1"></span><span class="path2"></span></span></a>
+      
+      <a href="#" class="bookmark"><span class="gema75_read_it_later_text addToReadItLaterButton" data-readitlater-id="<?php echo $post->ID; ?>"><span class="icon-bookmark_full"><span class="path1"></span><span class="path2"></span></span></span></a>
 
       <a href="<?php the_permalink();?>">
           <?php get_terms(); ?>
