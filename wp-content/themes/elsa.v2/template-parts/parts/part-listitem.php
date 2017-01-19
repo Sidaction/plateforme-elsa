1c0ad124a5ecfe5a2abbaf23d153237684ac96a7
Thomas <hello@thomasflorentin.net>
2017-01-19 18:08:00 +0100

    Polish list classes

--- wp-content/themes/elsa.v2/template-parts/parts/part-listitem.php
+++ wp-content/themes/elsa.v2/template-parts/parts/part-listitem.php
@@ -1,28 +1,38 @@
 <?php
-
-
+  $reco = get_field('recommandation');
+  $boite = get_the_term_list( $post->ID, 'boiteoutils', 'People: ', ', ' );
 ?>
 
 
-  <li class="search_item">
+  <li class="list_item">
 
 
-      <a href="#" class="removeFromRILButton search_item_bookmark" data-readitlater-id="<?php echo $post->ID ;?>" >
+      <a href="#" class="removeFromRILButton list_item-bookmark" data-readitlater-id="<?php echo $post->ID ;?>" >
         <span class="icon-bookmark_full"><span class="path1"></span><span class="path2"></span></span>
       </a>
 
-      <div class="search_item_inner">
+      <div class="list_item_inner">
+
+          <div class="list_item-icons">
+
+            <?php if( isset($reco) && $reco ) : ?>
+              <span class="icon-recommandation"></span>
+            <?php endif; ?>
+            <?php if( isset($boite) && $boite ) : ?>
+              <span class="icon-boite"></span>
+            <?php endif; ?>
+          </div>
 
           <a href="<?php the_permalink();?>?ref=search" class="linkProg">
-            <h4 class="h4 search_item-title"><?php the_title();?></h4>
+            <h4 class="h4 list_item-title"><?php the_title();?></h4>
           </a>
 
           <div class="row">
-            <div class="m-1col search_item-format">
+            <div class="m-1col list_item-format">
                 <span class="meta"><?php echo $format; ?></span>
             </div>
 
-            <div class="m-4col search_item-thematiques">
+            <div class="m-4col list_item-thematiques">
               <?php if( !empty($cat) ) : ?>
                 <div><span class="meta">Thématiques : </span>
                 <?php echo $cat ;?></div>
@@ -34,7 +44,7 @@
               <?php endif; ?>
             </div>
 
-            <div class="m-3col search_item-auteurs">
+            <div class="m-3col list_item-auteurs">
               <?php                              
               if(!empty($main_author)){ ?>
                 <span class="meta">Auteur(s) : </span>
