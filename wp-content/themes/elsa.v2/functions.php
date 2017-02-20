37ef4727c365ef57c9afa142a348a25240b63ac3
Thomas <hello@thomasflorentin.net>
2017-02-20 18:27:06 +0100

    Add func

--- wp-content/themes/elsa.v2/functions.php
+++ wp-content/themes/elsa.v2/functions.php
@@ -259,9 +259,6 @@ function elsa_scripts() {
 
     wp_dequeue_script('jquery-migrate');
     wp_dequeue_script('jquery-position');
-    wp_dequeue_script('wysija-validator');
-
-
 
     wp_enqueue_script( 'elsa-scripts' );
 
@@ -484,3 +481,19 @@ if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 
     session_destroy();   // destroy session data in storage
 }
 $_SESSION['LAST_ACTIVITY'] = time(); // update last activity time stamp
+
+
+
+function wp_get_attachment( $attachment_id ) {
+
+    $attachment = get_post( $attachment_id );
+    return array(
+        'alt' => get_post_meta( $attachment->ID, '_wp_attachment_image_alt', true ),
+        'caption' => $attachment->post_excerpt,
+        'description' => $attachment->post_content,
+        'href' => get_permalink( $attachment->ID ),
+        'src' => $attachment->guid,
+        'title' => $attachment->post_title
+    );
+}
+
