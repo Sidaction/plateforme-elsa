ab058f581f9b0b9d14757338241be81b2f933576
thomas <hello@thomasflorentin.net>
2021-02-17 15:52:55 +0100

    Polish

--- wp-content/themes/elsa.v2/template-parts/parts/part-bloc-image.php
+++ wp-content/themes/elsa.v2/template-parts/parts/part-bloc-image.php
@@ -14,70 +14,10 @@
 
 
 
-<?php if( $type == 'media' ) : ?>
-  <div class="bloc_item bloc--<?php echo $type; ?> bg_cover" style="background-image:url(<?php if ( has_post_thumbnail() ) { the_post_thumbnail_url(); }  ?>)">
-
-
-      <?php echo $Bookmarks->show_bookmark_btn(); ?>
-
-      <a href="<?php the_permalink(); ?><?php if( isset($ref) && $ref == 'media' ) echo '?ref=media'; ?>">
-        <span class="bloc-media_format icon_media ic-<?php echo $format; ?>"></span>
-      </a>
-
-      <a href="<?php the_permalink(); ?><?php if( isset($ref) && $ref == 'media' ) echo '?ref=media'; ?>">
-
-          <div class="bloc_meta bloc_category">
-            <?php 
-              if( $cat != '') {
-                limit_words($cat, 5); 
-              }
-              if ($format != ''  & $cat != '') {
-                echo ' | ';
-              }
-              if ($format != '') {
-                echo $format;
-              }
-              if ($format != ''  & $auteurs != '') {
-                echo ' | ';
-              }
-              if ($auteurs != '') {
-                echo limit_words($auteurs, 5);
-              }
-            ?>
-          </div>
-
-
-          <div>
-            <h2 class="h2 bloc_title">
-              <?php limit_words( get_the_title(), 7 );?>
-              <?php if( is_home() || is_front_page() ) {
-                echo '<p><a class="btn-inline" href="/medias">Voir tous nos médias</a></p>';
-                } ?>
-            </h2>
-          </div>
-
-            <div class="bloc_icons">
-              <?php if( isset($reco) && $reco ) : ?>
-                <span class="icon-recommandation"></span>
-              <?php endif; ?>
-              <?php if( isset($boite) && $boite ) : ?>
-                <span class="icon-boite"></span>
-              <?php elseif( isset($tools) && $tools === '1' ) : ?>
-                <span class="icon-boite"></span>
-              <?php endif; ?>
-            </div>
-
-
-      </a> 
-  </div>
-
-
-<?php else : ?>
-
   <div class="bloc_item--image bloc--<?php echo $type; ?>">
 
           <figure class="bloc_img">
-            <?php echo the_post_thumbnail(); ?>
+            <?php echo the_post_thumbnail('post_thumb'); ?>
           </figure>
 
 
@@ -133,8 +73,6 @@
       </a> 
     
   </div>
-
-<?php endif; ?>
 
 
   
