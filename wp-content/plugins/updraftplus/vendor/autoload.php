331015c72379416b8f3c9e0b39923b4f093077d4
thomas <hello@thomasflorentin.net>
2022-01-24 09:33:08 +0100

    Update Updraft Version 1.16.25

--- wp-content/plugins/updraftplus/vendor/autoload.php
+++ wp-content/plugins/updraftplus/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInitaab3e972f7bd452d7b6aec134b9a00fb::getLoader();
+return ComposerAutoloaderInit63f97d3a38955127174e9f4053424f74::getLoader();
