ecedb15d8589723056560eb63afa9d90ae673261
Alexis <90972106+alexishadjian@users.noreply.github.com>
2024-10-14 17:06:29 +0200

    style: footer

--- wp-content/themes/elsa.v3/footer.php
+++ wp-content/themes/elsa.v3/footer.php
@@ -1,8 +1,8 @@
 
 
-<footer id="mastfooter" class="site-footer">
+<!-- <footer id="mastfooter" class="site-footer"> -->
 
-    <div class="wrap row">
+    <!-- <div class="wrap row">
         <div class="m-4col">
             <img src="<?php echo get_template_directory_uri() ?>/assets/img/Logo_ELSA-Version2019_500px.png" width="86" height="auto" alt="logo ELSA" class="site-logo">
             <h3 class="site-title">Plateforme ELSA</h3>
@@ -72,14 +72,52 @@
 
 
 
-    </div>
+    </div> -->
     
 
+<!-- </footer> -->
+
+
+
+<footer class="site-footer">
+    <div class="wrapper">
+        
+        <?php the_field('elsa_text', 'options'); ?>
+        
+        <div class="bottom flex gap-xl">
+            <div class="site-logo">
+                <a href="<?php echo esc_url( home_url() ) ?>"><img src="<?php echo get_template_directory_uri() ?>/assets/img/logo-sidaction-white.png" width="auto" height="auto" alt="logo ELSA"></a>
+            </div>
+
+            <!-- <?php wp_nav_menu( array( 'theme_location' => 'footer', 'menu_id' => 'tertiary-menu' ) ); ?> -->
+            <ul class="footer-menu no-bullets">
+                <li class="menu-item"><a href="/soumettre-une-ressource" class="btn-inline">Nous soumettre une ressource</a> </li>
+                <li class="menu-item"><a href="/contactez-nous" class="btn-inline">Nous écrire</a> </li>
+                <li class="menu-item social_links">
+                    <span href="#" class="btn-inline">Nous suivre sur les réseaux</span>
+                    <a href="<?php the_field('url_facebook', 'options'); ?>" target="_blank" class="social_icon icon-facebook"></a>
+                    <a href="<?php the_field('url_twitter', 'options'); ?>" target="_blank" class="social_icon icon-twitter"></a>
+                </li>
+
+                <li class="menu-item js-newsletter-trigger"><a href="#" class="btn-inline">Recevoir notre newsletter</a> </li>
+
+            </ul>
+
+            <?php wp_nav_menu( array( 
+                'theme_location'    => 'bottom', 
+                'menu_id'           => 'bottom-menu',   
+                'items_wrap'        => '<ul id="%1$s" class="%2$s">%3$s <a href="#" id="tarteaucitronManager" class="menu-item"> Cookies</a></ul>', 
+            )); ?>
+
+
+        <div>
+    </div>
+                
     <?php wp_footer();?>
 </footer>
 
 
-    
+<!--     
 <div class="modal modal-newsletter">
     <div class="modal_inner wrap">
 
@@ -94,10 +132,10 @@
         </div>
         
     </div>  
-</div>
+</div> -->
 
 
-<div id="empty_modal" class="modal modal-empty">
+<!-- <div id="empty_modal" class="modal modal-empty">
     <div class="modal_inner wrap">
         <a href="#" id="" class="modal_close">
             <span class="icon-close"></span>
@@ -111,7 +149,7 @@
 
         </div>
     </div>
-</div>
+</div> -->
 
 
 <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.5.18/webfont.js"></script>
