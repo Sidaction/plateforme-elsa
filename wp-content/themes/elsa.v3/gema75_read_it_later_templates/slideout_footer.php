9f680274e024e2f0519004b565d885ec0bc5569e
Thomas <hello@thomasflorentin.net>
2024-10-14 10:19:37 +0200

    remove Unused files

--- wp-content/themes/elsa.v3/gema75_read_it_later_templates/slideout_footer.php
+++
@@ -1,3 +0,0 @@
-     
-<div style="float:left;" id="gema75_ril_list" class="owl-carousel"></div>
-</div>
--- wp-content/themes/elsa.v3/gema75_read_it_later_templates/slideout_header.php
+++
@@ -1,2 +0,0 @@
-<div class="slide-out-div">
-  <a class="handle"><?php echo $slideout_variables['slideout_tab_header'] ;?><?php echo $slideout_variables['slideout_tab_counter'] ;?></a>      
