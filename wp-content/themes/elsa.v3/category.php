48cfd3ddb954da72cad860017c1100bca59fc2c7
Alexis <90972106+alexishadjian@users.noreply.github.com>
2024-10-18 14:43:38 +0200

    style: category content

--- wp-content/themes/elsa.v3/category.php
+++ wp-content/themes/elsa.v3/category.php
@@ -64,191 +64,70 @@ get_header();
             <?php endif; ?>
         </div>
     </section>
+    
+    <section class="sec_category-content">
+        <div class="grid gap-xl wrapper">
+            <div class="s-9col entry-content">
+                <?= wpautop($presentation); ?>
+            </div>
+            <div class="s-3col flex column end-y">
+                <a href="#recommandations" class="btn btn--secondary mb-m">Nos recommandations</a>
+                <a href="/recherche-documentaire/?totalcat=<?php echo $cat_slug; ?>" class="btn">Toutes les ressources</a>
+            </div>
+        </div>
+    </section>
+
+    <section class="sec_related-ressources">
+        <div class="wrapper">
+            <h2 class="h2 mb-l">Ressources en lien</h2>
+            <div class="flex column gap-l mb-l">
+                <?php
+                $related = get_posts(array(
+                    'category__in' => wp_get_post_categories($post->ID),
+                    'numberposts' => 3,
+                    'post__not_in' => array($post->ID)
+                ));
+
+                if ($related) {
+                    foreach ($related as $post) {
+                        setup_postdata($post); 
+                        $format = cnLib::get_main_term_slug($post->ID, 'format');
+                        
+                        ?>
+                        <div class="ressource-item">
+
+                            <?php if (!empty($format)) : ?>
+                                <span class="ressource-item__format"><?= $format ?></span>
+                            <?php endif; ?>
+
+                            <h4 class="ressource-item__title h4"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
+                            <p class="ressource-meta small"><span>Thématiques : </span><?php the_category(', '); ?></p>
+                            
+                            <?php if( !empty($main_author) || $cnSite->get_authors($post->ID) !== ''){ ?>
+                                <p class="ressource-meta small">
+                                    <span>Auteur(s) : </span>
+                                    <?php $permalink = get_permalink( $main_author );
+                                    if(!empty($url)) echo "<a href='{$permalink}'>{$main_author}</a>"; ?>
+                                    <?php echo $cnSite->get_authors($post->ID); ?>
+                                </p>
+                            <?php } ?>
+
+                        </div>
+                    <?php }
+                    wp_reset_postdata();
+                } else {
+                    echo '<p>Aucune ressource liée trouvée.</p>';
+                }
+                ?>
+            </div>
+            
+            <a href="/recherche-documentaire/?totalcat=<?php echo $cat_slug; ?>" class="btn btn--primary">Voir toutes les ressources</a>
+        </div>
+    </section>
+
 </main>
 
 
 
 
-
-    <section id="site-content" class="site-content">
-
-        <article class="main-content clearfix">
-            <div class="page_title ressource_title">
-
-                <div class="wrap row">
-                    <h1 class="h1 m-5col m-clearfix">
-                        <?php echo single_cat_title("", false); ?>
-                    </h1>  
-                </div>     
-            </div>
-
-
-            <div class="page_content clearfix">
-                <div class="wrap row">
-                    <div class="m-5col page_main page_copy">
-                        <?php echo wpautop($presentation); ?>
-                    </div>
-
-                    <div class="m-3col page_aside m-last">
-
-                        <?php if( $image ) { ?>
-                            <div class="page_media clearfix">
-                                <?php echo wp_get_attachment_image( $image, $size ); ?>
-                            </div>
-                        <?php } ?>
-
-
-                        <?php if( isset($tags_linked) || isset($boites_linked) ) : ?>
-                                <div class="page_metas">
-
-                                    <?php if( isset($boites_linked) && !empty($boites_linked) ) : ?>
-                                        <div class="page_metas_row">
-                                            <span>Boites à outils : </span>
-
-                                            <ul>
-
-                                            <?php foreach( $boites_linked as $term ): ?>
-                                                <li><a href="<?php echo get_term_link( $term ); ?>"><?php echo $term->name; ?></a></li>
-                                            <?php endforeach; ?>
-
-                                            </ul>
-
-                                        </div>
-                                    <?php endif; ?>
-
-                                    <?php if( isset($tags_linked) && !empty($tags_linked) ) : ?>
-                                        <div class="page_metas_row">
-                                            <span>Mots clefs : </span>
-
-                                            <ul>
-
-                                            <?php foreach( $tags_linked as $term ): ?>
-                                                <li><a href="<?php echo get_term_link( $term ); ?>"><?php echo $term->name; ?></a></li>
-                                            <?php endforeach; ?>
-
-                                            </ul>
-
-                                        </div>
-                                    <?php endif; ?>
-
-                                </div>
-                        <?php endif; ?>
-
-                        <div class="page_actions">
-                            <a href="#recommandations" class="scroll btn-primary plain">Les ressources recommandées</a>
-                            <a href="/recherche-documentaire/?totalcat=<?php echo $cat_slug; ?>" class="btn-secondary plain">Toutes les ressources de la thématique</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </article>
-
-            
-        <div id="" class="blocs_group bg-cut">
-            <div class="wrap row">
-
-                <div class="group_title grid-title m-2col">
-                    <h3 id="recommandations" class="h3_alt">La plateforme ELSA vous recommande</h3>
-                </div>
-
-
-                <div class="group_list grid-list">
-         
-                <?php
-                	$args = array(
-                        'post_type' => array('post'), 
-                        'posts_per_page' => 5, 
-                        'cat' =>  $cat_id, 
-                        'meta_query' => array(
-                            array(
-                                'key' => 'homefiche',
-                                'compare' => '==',
-                                'value' => '1'
-                            )
-                        )
-                    );
-			        $grille_posts = get_posts($args);
-    
-                    if( !empty( $grille_posts ) ) :
-                        $i = 0; 
-
-                        foreach ( $grille_posts as $post ) : setup_postdata( $post ); ?>
-
-                                <?php 
-                                    $format = cnLib::get_main_term_slug($post->ID, 'format');
-                                    switch ($format) {
-                                        case 'video':
-                                            $type = 'media';
-                                            break;
-                                        case 'audio':
-                                            $type = 'media';
-                                            break;
-                                        case 'diaporama':
-                                            $type = 'media';
-                                            break;
-                                        default:
-                                            $type = 'ressource';
-                                            break;
-                                    }
-                                ?>
-
-                                <?php if( $i == 0 ) : ?>
-                                    <div class="m-2col">
-                                        <?php set_query_var( 'type', $type ); ?>
-                                        <?php set_query_var( 'cnSite', $cnSite ); ?>
-
-                                <?php elseif ( $i % 2 == 0 ) : ?>
-                                    <div class="m-4col m-clearfix">
-                                        <?php set_query_var( 'type', $type ); ?>
-                                        <?php set_query_var( 'cnSite', $cnSite ); ?>
-
-                                <?php else : ?>
-                                    <div class="m-4col">
-                                        <?php set_query_var( 'type', $type ); ?>
-
-                                <?php endif; ?>
-
-                                        <?php get_template_part('template-parts/parts/part', 'bloc'); ?>
-
-                                    </div><!-- end .col -->
-
-
-                            <?php $i++; ?>
-                                
-
-                            <?php endforeach; 
-                            wp_reset_postdata(); ?>
-
-
-                            </div>
-
-                            <div class="row group_action">
-                                <a href="/recherche-documentaire/?totalcat=<?php echo $cat_slug; ?>" class="btn-secondary is-centered">Toutes les ressources de la thématique</a>
-                            </div> 
-
-                            <?php else : ?>
-                                <div class="m-6col">
-                                    <p><br>Il n'y a aucune ressource recommandée pour cette thématique....</p>
-                                    <a href="/recherche-documentaire/" class="btn-secondary is-centered">Toutes les ressources</a>
-                                </div>
-
-                            </div>
-
-
-                            <?php endif; ?>
-
-                    
-                </div><!-- .wrap -->
-
-            </div><!-- .blocs_group -->
-
-         </div>
-     </div>
-
-
-    <?php get_template_part('template-parts/content', 'rebonds'); ?>
-
-
-</section>
 <?php get_footer(); ?>
-
