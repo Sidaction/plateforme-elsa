62d1f6f7afd2aa6b1fbbafbda150d95e18d521f3
Alexis <90972106+alexishadjian@users.noreply.github.com>
2024-10-25 15:24:27 +0200

    feat: page 404

--- wp-content/themes/elsa.v3/404.php
+++ wp-content/themes/elsa.v3/404.php
@@ -2,8 +2,12 @@
 
 get_header(); ?>
 
-<div class="wrap">
-<h3>oops, nothing here...</h3>
-</div>
+<main class="flex center-y">
+    <div class="wrapper flex column center-y h-full gap-m">
+        <h1>404</h1>
+        <h4 class="txt-center">La page que vous cherchez n'existe pas.</h4>
+        <a href="<?php echo get_home_url(); ?>" class="btn">Accueil</a>
+    </div>
+</main>
 
 <?php get_footer(); ?>
