f587995a3192700caa555cb32a91bcdb42013f44
Thomas <hello@thomasflorentin.net>
2024-11-07 11:11:18 +0100

    svg: Cleaning

---
+++ wp-content/themes/elsa.v3/svg/svg-download.php
@@ -0,0 +1,9 @@
+<?php $strokes = $args['strokes'] ? $args['strokes'] : '#fff'; ?>
+
+<svg width="29" height="30" viewBox="0 0 29 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M16.9165 4.125V8.95833C16.9165 9.2788 17.0438 9.58615 17.2704 9.81275C17.497 10.0394 17.8044 10.1667 18.1248 10.1667H22.9582" stroke="<?php echo $strokes; ?>" stroke-width="1.75" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M20.5415 25.875H8.45817C7.81723 25.875 7.20254 25.6204 6.74933 25.1672C6.29612 24.714 6.0415 24.0993 6.0415 23.4583V6.54167C6.0415 5.90073 6.29612 5.28604 6.74933 4.83283C7.20254 4.37961 7.81723 4.125 8.45817 4.125H16.9165L22.9582 10.1667V23.4583C22.9582 24.0993 22.7036 24.714 22.2503 25.1672C21.7971 25.6204 21.1824 25.875 20.5415 25.875Z" stroke="<?php echo $strokes; ?>" stroke-width="1.75" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M14.5 21.0416V13.7916" stroke="<?php echo $strokes; ?>" stroke-width="1.75" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M11.479 18.0209L14.4998 21.0417L17.5207 18.0209" stroke="<?php echo $strokes; ?>" stroke-width="1.75" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+
