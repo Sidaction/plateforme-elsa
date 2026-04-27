0e40237f84b2a3fe8558455336a79c7b5ccc6158
Thomas <hello@thomasflorentin.net>
2026-04-27 08:32:55 +0200

    updates: Meta Box

--- wp-content/plugins/meta-box/vendor/composer/installed.php
+++ wp-content/plugins/meta-box/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'wpmetabox/meta-box',
-        'pretty_version' => '5.11.3',
-        'version' => '5.11.3.0',
-        'reference' => 'ba3573605781ada11c77765955b22e2fbf9ed9ac',
+        'pretty_version' => '5.12.0',
+        'version' => '5.12.0.0',
+        'reference' => 'ab68b5ee5949640b37608d1056845d79bf029d2f',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'wpmetabox/meta-box' => array(
-            'pretty_version' => '5.11.3',
-            'version' => '5.11.3.0',
-            'reference' => 'ba3573605781ada11c77765955b22e2fbf9ed9ac',
+            'pretty_version' => '5.12.0',
+            'version' => '5.12.0.0',
+            'reference' => 'ab68b5ee5949640b37608d1056845d79bf029d2f',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
